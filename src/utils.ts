a99e5de17ad5be91260096d849968181b2367cbe
ghosty2004 <me@ghosty2004.dev>
2024-07-07 09:43:41 +0300

    feat: add function capitalizeFirstWord

--- src/utils.ts
+++ src/utils.ts
@@ -5,3 +5,9 @@ export const celsiusToFahrenheit = (celsius: number) => {
 export const fahrenheitToCelsius = (fahrenheit: number) => {
   return ((fahrenheit - 32) * 5) / 9;
 };
+
+export const capitalizeFirstWord = (str: string) =>
+  str
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
